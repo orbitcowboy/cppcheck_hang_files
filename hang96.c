41624cc20627ba895e8ee378e1dff9f87223f003
orbitcowboy <ettl.martin78@gmail.com>
2015-12-10 16:27:21 +0100

    Added another hang.

---
+++ hang96.c
@@ -0,0 +1,9 @@
+return <<  >>  x return <<  >>  x 
+
+/*Checking hang96.c...
+^C
+Program received signal SIGINT, Interrupt.
+0x0000000000885777 in CheckOther::checkNegativeBitwiseShift (
+    this=0x7fffffffbbd0) at lib/checkother.cpp:2206
+2206	        for (const Token *parent = tok; parent; parent = parent->astParent()) {
+*/
